80da576789f68123081247fcea6a39cbe5335e25
LaupWing <loc_strict@hotmail.com>
2022-05-08 16:38:50 +0200

    okayyyy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,33 @@
 const fs = require('fs')
-const {ethers, artifacts} = require('hardhat')
+const { ethers, artifacts } = require('hardhat')
 
-async function main(){
+async function main() {
    const [deployer, user1] = await ethers.getSigners()
    const DecentratwitterFactory = await ethers.getContractFactory('Decentratwitter')
    const decentratwitter = await DecentratwitterFactory.deploy()
    const contractDir = __dirname + '/../src/contractsData'
 
-   if(!fs.existsSync(contractDir)){
+   if (!fs.existsSync(contractDir)) {
       fs.mkdirSync(contractDir)
    }
 
-   const contractArtifact = artifacts.readArtifact("Decentratwitter")
-
    fs.writeFileSync(
-      contractDir + '/decentratwitter-address.json',
-      JSON.stringify(contractArtifact, null, 2)
+      contractDir + `/decentratwitter-address.json`,
+      JSON.stringify({ address: decentratwitter.address }, undefined, 2)
    )
 
-   console.log(`Deployed to : ${decentratwitter.address}`)
+   const contractArtifact = artifacts.readArtifactSync("Decentratwitter");
+
+   fs.writeFileSync(
+      contractDir + `/decentratwitter.json`,
+      JSON.stringify(contractArtifact, null, 2)
+   )
+   console.log("Decentratwitter deployed to:", decentratwitter.address)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+   .then(() => process.exit(0))
+   .catch((error) => {
+      console.error(error);
+      process.exit(1);
+   });
