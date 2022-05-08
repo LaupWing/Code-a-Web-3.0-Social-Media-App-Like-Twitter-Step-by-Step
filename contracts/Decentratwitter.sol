199d1557433a04fcf5751e7a2ed8788849957a71
LaupWing <loc_strict@hotmail.com>
2022-05-08 17:15:43 +0200

    struct

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -5,5 +5,15 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Decentratwitter is ERC721URIStorage {
+   uint256 public tokenCount;
+   uint256 public postCount;
+
+   struct Post {
+      uint256 id;
+      string hash; //points to the location of the post
+      uint256 tipAmount;
+      address payable author;
+   }
+
    constructor() ERC721("Decentratwitter", "DAPP"){}
 }
